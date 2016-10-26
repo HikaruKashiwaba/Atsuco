a0307ed4ed40021edb5a3bc05a212f2036f0584a
HikaruKashiwaba <kashiwaba@ugatria.co.jp>
2016-10-26 21:58:51 +0900

    Initial Commit

---
+++ Atsuco/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Atsuco
+//
+//  Created by Kashiwaba Hikaru on 2016/10/26.
+//  Copyright © 2016年 Ugatria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
